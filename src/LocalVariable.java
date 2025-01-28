16cb1e1cedc06cc8f00c5e9b6044c180652f5243
ChaitanyaBuridi <162965249+Chaituxyzz@users.noreply.github.com>
2025-01-28 19:26:56 +0530

    LocalVariable

---
+++ src/LocalVariable.java
@@ -0,0 +1,14 @@
+package com.wiproNga;
+
+public class LocalVariable {
+
+	 public void display() {
+		 int localVariable = 20;
+		 System.out.println(localVariable);
+	 }
+	public static void main(String[] args) {
+		LocalVariable Lv = new LocalVariable();
+		Lv.display();
+	}
+
+}
